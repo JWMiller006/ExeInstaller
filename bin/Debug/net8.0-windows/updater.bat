386e25b50397664c38454a1c701c00a920bb4176
J Miller <jwmiller006@outlook.com>
2024-02-02 20:56:14 -0500

    finish self updating code

--- bin/Debug/net8.0-windows/updater.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-::%1 is the installation directory
-::%2 is the download directory
-sleep 5
-set dir=%1
-rmdir /s /q %dir%
-mkdir %dir%
-copy %2\*.* %dir%
-::del /f /s /q %1"\*.*"
--- bin/Release/net8.0-windows/updater.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-::%1 is the installation directory
-::%2 is the download directory
-sleep 5
-set dir=%1
-rmdir /s /q %dir%
-mkdir %dir%
-copy %2\*.* %dir%
