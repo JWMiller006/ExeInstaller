5976d12b6478f7373e5f7e2fcaf91b5f3fcde118
J Miller <jwmiller006@outlook.com>
2024-02-09 03:42:13 -0500

    Update Admin page

--- Pages/Administrative.Designer.cs
+++ Pages/Administrative.Designer.cs
@@ -40,23 +40,23 @@
             jsonToolStripMenuItem = new ToolStripMenuItem();
             txtToolStripMenuItem = new ToolStripMenuItem();
             otherToolStripMenuItem = new ToolStripMenuItem();
-            viewToolStripMenuItem = new ToolStripMenuItem();
-            helpToolStripMenuItem = new ToolStripMenuItem();
-            whatIsThisToolStripMenuItem = new ToolStripMenuItem();
-            whyAmISeeingThisToolStripMenuItem = new ToolStripMenuItem();
-            closeToolStripMenuItem = new ToolStripMenuItem();
             closeToolStripMenuItem1 = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
             smallWindowToolStripMenuItem = new ToolStripMenuItem();
             smallToolStripMenuItem = new ToolStripMenuItem();
             mediumToolStripMenuItem = new ToolStripMenuItem();
             largeToolStripMenuItem = new ToolStripMenuItem();
             taskManagerToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            whatIsThisToolStripMenuItem = new ToolStripMenuItem();
+            whyAmISeeingThisToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
             statusStrip1 = new StatusStrip();
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
+            mainContainer = new TabControl();
+            InstallPage = new TabPage();
+            VersionPage = new TabPage();
             menu.SuspendLayout();
-            tabControl1.SuspendLayout();
+            mainContainer.SuspendLayout();
             SuspendLayout();
             // 
             // menu
@@ -85,20 +85,20 @@
             // txtToolStripMenuItem1
             // 
             txtToolStripMenuItem1.Name = "txtToolStripMenuItem1";
-            txtToolStripMenuItem1.Size = new Size(104, 22);
+            txtToolStripMenuItem1.Size = new Size(180, 22);
             txtToolStripMenuItem1.Text = ".txt";
             // 
             // jsonToolStripMenuItem1
             // 
             jsonToolStripMenuItem1.Name = "jsonToolStripMenuItem1";
-            jsonToolStripMenuItem1.Size = new Size(104, 22);
+            jsonToolStripMenuItem1.Size = new Size(180, 22);
             jsonToolStripMenuItem1.Text = ".json";
             // 
             // otherToolStripMenuItem1
             // 
             otherToolStripMenuItem1.Name = "otherToolStripMenuItem1";
-            otherToolStripMenuItem1.Size = new Size(104, 22);
-            otherToolStripMenuItem1.Text = "Other";
+            otherToolStripMenuItem1.Size = new Size(180, 22);
+            otherToolStripMenuItem1.Text = "Other...";
             // 
             // saveToolStripMenuItem
             // 
@@ -131,12 +131,50 @@
             otherToolStripMenuItem.Size = new Size(104, 22);
             otherToolStripMenuItem.Text = "Other";
             // 
+            // closeToolStripMenuItem1
+            // 
+            closeToolStripMenuItem1.Name = "closeToolStripMenuItem1";
+            closeToolStripMenuItem1.Size = new Size(180, 22);
+            closeToolStripMenuItem1.Text = "Close";
+            // 
             // viewToolStripMenuItem
             // 
             viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { smallWindowToolStripMenuItem, taskManagerToolStripMenuItem });
             viewToolStripMenuItem.Name = "viewToolStripMenuItem";
             viewToolStripMenuItem.Size = new Size(44, 20);
             viewToolStripMenuItem.Text = "View";
+            // 
+            // smallWindowToolStripMenuItem
+            // 
+            smallWindowToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { smallToolStripMenuItem, mediumToolStripMenuItem, largeToolStripMenuItem });
+            smallWindowToolStripMenuItem.Name = "smallWindowToolStripMenuItem";
+            smallWindowToolStripMenuItem.Size = new Size(146, 22);
+            smallWindowToolStripMenuItem.Text = "Window";
+            smallWindowToolStripMenuItem.Click += smallWindowToolStripMenuItem_Click;
+            // 
+            // smallToolStripMenuItem
+            // 
+            smallToolStripMenuItem.Name = "smallToolStripMenuItem";
+            smallToolStripMenuItem.Size = new Size(119, 22);
+            smallToolStripMenuItem.Text = "Small";
+            // 
+            // mediumToolStripMenuItem
+            // 
+            mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            mediumToolStripMenuItem.Size = new Size(119, 22);
+            mediumToolStripMenuItem.Text = "Medium";
+            // 
+            // largeToolStripMenuItem
+            // 
+            largeToolStripMenuItem.Name = "largeToolStripMenuItem";
+            largeToolStripMenuItem.Size = new Size(119, 22);
+            largeToolStripMenuItem.Text = "Large";
+            // 
+            // taskManagerToolStripMenuItem
+            // 
+            taskManagerToolStripMenuItem.Name = "taskManagerToolStripMenuItem";
+            taskManagerToolStripMenuItem.Size = new Size(146, 22);
+            taskManagerToolStripMenuItem.Text = "Task Manager";
             // 
             // helpToolStripMenuItem
             // 
@@ -163,44 +201,6 @@
             closeToolStripMenuItem.Size = new Size(188, 22);
             closeToolStripMenuItem.Text = "Close";
             // 
-            // closeToolStripMenuItem1
-            // 
-            closeToolStripMenuItem1.Name = "closeToolStripMenuItem1";
-            closeToolStripMenuItem1.Size = new Size(180, 22);
-            closeToolStripMenuItem1.Text = "Close";
-            // 
-            // smallWindowToolStripMenuItem
-            // 
-            smallWindowToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { smallToolStripMenuItem, mediumToolStripMenuItem, largeToolStripMenuItem });
-            smallWindowToolStripMenuItem.Name = "smallWindowToolStripMenuItem";
-            smallWindowToolStripMenuItem.Size = new Size(180, 22);
-            smallWindowToolStripMenuItem.Text = "Window";
-            smallWindowToolStripMenuItem.Click += smallWindowToolStripMenuItem_Click;
-            // 
-            // smallToolStripMenuItem
-            // 
-            smallToolStripMenuItem.Name = "smallToolStripMenuItem";
-            smallToolStripMenuItem.Size = new Size(180, 22);
-            smallToolStripMenuItem.Text = "Small";
-            // 
-            // mediumToolStripMenuItem
-            // 
-            mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
-            mediumToolStripMenuItem.Size = new Size(180, 22);
-            mediumToolStripMenuItem.Text = "Medium";
-            // 
-            // largeToolStripMenuItem
-            // 
-            largeToolStripMenuItem.Name = "largeToolStripMenuItem";
-            largeToolStripMenuItem.Size = new Size(180, 22);
-            largeToolStripMenuItem.Text = "Large";
-            // 
-            // taskManagerToolStripMenuItem
-            // 
-            taskManagerToolStripMenuItem.Name = "taskManagerToolStripMenuItem";
-            taskManagerToolStripMenuItem.Size = new Size(180, 22);
-            taskManagerToolStripMenuItem.Text = "Task Manager";
-            // 
             // statusStrip1
             // 
             statusStrip1.Location = new Point(0, 528);
@@ -209,35 +209,36 @@
             statusStrip1.TabIndex = 1;
             statusStrip1.Text = "statusStrip1";
             // 
-            // tabControl1
+            // mainContainer
             // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(0, 27);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1090, 498);
-            tabControl1.TabIndex = 2;
+            mainContainer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            mainContainer.Controls.Add(InstallPage);
+            mainContainer.Controls.Add(VersionPage);
+            mainContainer.Location = new Point(0, 27);
+            mainContainer.Name = "mainContainer";
+            mainContainer.SelectedIndex = 0;
+            mainContainer.Size = new Size(1090, 498);
+            mainContainer.TabIndex = 2;
             // 
-            // tabPage1
+            // InstallPage
             // 
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1082, 470);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
-            tabPage1.UseVisualStyleBackColor = true;
+            InstallPage.BackColor = Color.PaleVioletRed;
+            InstallPage.Location = new Point(4, 24);
+            InstallPage.Name = "InstallPage";
+            InstallPage.Padding = new Padding(3);
+            InstallPage.Size = new Size(1082, 470);
+            InstallPage.TabIndex = 0;
+            InstallPage.Text = "Register Application";
             // 
-            // tabPage2
+            // VersionPage
             // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(192, 72);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            VersionPage.BackColor = Color.LightPink;
+            VersionPage.Location = new Point(4, 24);
+            VersionPage.Name = "VersionPage";
+            VersionPage.Padding = new Padding(3);
+            VersionPage.Size = new Size(1082, 470);
+            VersionPage.TabIndex = 1;
+            VersionPage.Text = "Current Version Info";
             // 
             // Administrative
             // 
@@ -245,7 +246,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.IndianRed;
             ClientSize = new Size(1090, 550);
-            Controls.Add(tabControl1);
+            Controls.Add(mainContainer);
             Controls.Add(statusStrip1);
             Controls.Add(menu);
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -254,7 +255,7 @@
             Text = "Admin Page";
             menu.ResumeLayout(false);
             menu.PerformLayout();
-            tabControl1.ResumeLayout(false);
+            mainContainer.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -284,8 +285,8 @@
         private ToolStripMenuItem largeToolStripMenuItem;
         private ToolStripMenuItem taskManagerToolStripMenuItem;
         private StatusStrip statusStrip1;
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private TabControl mainContainer;
+        private TabPage InstallPage;
+        private TabPage VersionPage;
     }
 }
